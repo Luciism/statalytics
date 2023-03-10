c59a25af6f1edaf62ee3fe9c767754de3e69e67b
Depleted <odepleted@gmail.com>
2023-03-10 15:29:36 +1300

    Fixed custom background database location

--- bot/custombackground.py
+++ bot/custombackground.py
@@ -11,8 +11,7 @@ def background(path, uuid, default):
         return f'{path}/{default}.png'
     discordid = linked_data[1]
 
-    parent_dir = os.path.dirname(os.path.abspath(os.getcwd()))
-    with sqlite3.connect(f'{parent_dir}/statalyticsWeb/subscriptions.db') as conn:
+    with sqlite3.connect(f'{os.getcwd()}/database/subscriptions.db') as conn:
         cursor = conn.cursor()
         cursor.execute(f"SELECT * FROM subscriptions WHERE discordid = '{discordid}'")
         subscription = cursor.fetchone()
