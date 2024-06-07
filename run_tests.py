b6c8c616b8fcac5eb062a3b14fb89b59fc4265e5
Lucism <lucism.dev@gmail.com>
2024-06-07 12:08:41 +1200

    tests: exit with non zero code if one or more tests fail

--- run_tests.py
+++ run_tests.py
@@ -1,11 +1,19 @@
 import unittest
 
+
 loader = unittest.TestLoader()
+
+failures = {"count": 0}
 
 def run_test_suite(start_dir: str) -> None:
     suite = loader.discover(start_dir)
     print(f"Running: {start_dir}")
-    unittest.TextTestRunner().run(suite)
+    test_result = unittest.TextTestRunner().run(suite)
+
+    failures["count"] += len(test_result.failures)
 
 run_test_suite("tests/test_statalib")
 run_test_suite("tests/test_statalib/test_rotational")
+
+if failures["count"] > 0:
+    exit(1)
