05ec885c9c531fc3dabc840e88154eb42a0b0423
Depleted <odepleted@gmail.com>
2025-01-01 06:57:15 +1300

    feat: add additional growth metrics functionality

--- statalib/usage.py
+++ statalib/usage.py
@@ -1,7 +1,8 @@
 """Usage metrics related functionality."""
 
 import time
-from typing import Literal
+from datetime import datetime, UTC
+from typing import Literal, TypedDict
 
 from .db import ensure_cursor, Cursor
 
@@ -23,7 +24,7 @@ def insert_growth_data(
     :param timestamp: The timestamp of the growth (defaults to now).
     """
     if timestamp is None:
-        timestamp = time.time()
+        timestamp = datetime.now(UTC).timestamp()
 
     cursor.execute(
         'INSERT INTO growth_data '
@@ -98,3 +99,106 @@ def get_commands_total(*, cursor: Cursor=None) -> int:
     if result:
         return result[0]
     return 0
+
+
+@ensure_cursor
+def get_growth_difference(
+    seconds: int,
+    metric: Literal['guild', 'user', 'linked'],
+    *, cursor: Cursor=None
+) -> int:
+    """
+    Get the difference in a growth metric from the last `seconds` seconds.
+
+    :param seconds: The number of seconds to get the difference from.
+    :param metric: The metric to get the difference from.
+    :return int: The difference in the metric.
+    """
+    since = datetime.now(UTC).timestamp() - seconds
+
+    growth_data = cursor.execute(
+        'SELECT action FROM growth_data '
+        'WHERE growth = ? and timestamp > ? '
+        'ORDER BY timestamp DESC',
+        (metric, since)
+    ).fetchall()
+
+    # Calculate the difference based on the type of entry
+    diff = 0
+    for grow in growth_data:
+        if grow[0] == 'add':
+            diff += 1
+        else:
+            diff -= 1
+
+    return diff
+
+
+class TimestampsSinceDict(TypedDict):
+    timestamp: float
+    action: str
+
+
+class Metrics:
+    def __init__(self, metric: Literal['guild', 'user', 'linked']) -> None:
+        self.metric = metric
+
+    @ensure_cursor
+    def get_timestamps_since(
+        self, seconds_ago: int, *, cursor: Cursor=None
+    ) -> list[TimestampsSinceDict]:
+        timestamp = datetime.now(UTC).timestamp() - seconds_ago
+
+        results = cursor.execute(
+            'SELECT timestamp, action FROM growth_data '
+            'WHERE growth = ? and timestamp > ? '
+            'ORDER BY timestamp DESC',
+            (self.metric, timestamp)
+        ).fetchall()
+
+        return [{'timestamp': row[0], 'action': row[1]} for row in results]
+
+
+    def group_timestamps(
+        self,
+        timestamps_1: list[float],
+        timestamps_2: list[float] | None=None,
+        date_format: str="%b %d, %Y"
+    ) -> tuple[list[str], list[float]] | tuple[list[str], list[float], list[float]]:
+        grouped_data: dict[str, dict[str, int]] = {}
+
+        # Create a datetime object for each timestamp
+        datetimes_1 = [datetime.fromtimestamp(ts, UTC) for ts in timestamps_1]
+        all_datetimes = sorted(datetimes_1)
+
+        # Do the same for timestamps 2 if it is passed
+        if timestamps_2 is not None:
+            datetimes_2 = [datetime.fromtimestamp(ts, UTC) for ts in timestamps_2]
+            all_datetimes.extend(datetimes_2)
+            all_datetimes.sort()
+
+        # Group the timestamps by day
+        for dt in all_datetimes:
+            # Round the timestamp to the nearest 24 hours
+            day_dt = dt.replace(
+                hour=(dt.hour // 24) * 24, minute=0, second=0, microsecond=0)
+
+            date_str = day_dt.strftime(date_format)
+            grouped_data.setdefault(date_str, {'1': 0, '2': 0})
+
+            # Increment the count for the day
+            if dt in datetimes_1:
+                grouped_data[date_str]['1'] += 1
+
+            if timestamps_2 is not None and dt in datetimes_2:
+                grouped_data[date_str]['2'] += 1
+
+        # Return the grouped data
+        formatted_timestamps = list(grouped_data.keys())
+        data_count_1 = [grouped_data[key]['1'] for key in grouped_data]
+
+        if datetimes_2 is None:
+            return formatted_timestamps, data_count_1
+
+        data_count_2 = [grouped_data[key]['2'] for key in grouped_data]
+        return formatted_timestamps, data_count_1, data_count_2
