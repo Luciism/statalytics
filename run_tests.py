1c7cc306f94afe138fc08cdb5da89e58ef2368c9
Depleted <odepleted@gmail.com>
2025-01-05 02:48:10 +1300

    test: use seperate loader per test sutie

--- run_tests.py
+++ run_tests.py
@@ -6,20 +6,20 @@ from statalib.common import REL_PATH
 from dotenv import load_dotenv
 load_dotenv(f"{REL_PATH}/.env.test")
 
-loader = unittest.TestLoader()
-
 failures = 0
 
 def run_test_suite(start_dir: str) -> int:
+    loader = unittest.TestLoader()
     suite = loader.discover(start_dir)
+
     print(f"Running: {start_dir}")
     test_result = unittest.TextTestRunner().run(suite)
 
     return len(test_result.failures)
 
 failures += run_test_suite("tests/test_statalib")
-# if os.path.exists("apps/website/tests"):
-#     failures += run_test_suite("apps/website/tests/")
+if os.path.exists("apps/website/tests"):
+    failures += run_test_suite("apps/website/tests/")
 
 if failures > 0:
     exit(1)
