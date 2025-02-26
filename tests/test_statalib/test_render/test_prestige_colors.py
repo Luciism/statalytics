7c849c21d60dc2ba5eb003979ff04816a2d73d00
Luciism <lucism.dev@gmail.com>
2025-02-27 06:34:51 +1300

    docs: add todo comment to tests

--- tests/test_statalib/test_render/test_prestige_colors.py
+++ tests/test_statalib/test_render/test_prestige_colors.py
@@ -2,7 +2,7 @@ import unittest
 
 from statalib.render import Prestige, PrestigeColors, BackgroundImageLoader, ImageRender
 
-
+# TODO
 class TestPrestigeColorFormatting(unittest.TestCase):
     def test_format_level_0(self):
         self.assertEqual(Prestige(0).formatted_level, "&7[0✫]")
