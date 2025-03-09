f5a6a466f1f987edde98eb6f7bc5263b7b4e722d
Luciism <lucism.dev@gmail.com>
2025-03-09 14:32:47 +1300

    build: revert to python 3.11 & switch to uv

--- flake.nix
+++ flake.nix
@@ -1,8 +1,37 @@
+# {
+#   description = "Construct development shell from requirements.txt";
+#
+#   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+#
+#   inputs.pyproject-nix.url = "github:pyproject-nix/pyproject.nix";
+#
+#   outputs =
+#     { nixpkgs, pyproject-nix, ... }:
+#     let
+#       # Load/parse requirements.txt
+#       project = pyproject-nix.lib.project.loadRequirementsTxt { projectRoot = ./dev;  };
+#
+#       pkgs = nixpkgs.legacyPackages.x86_64-linux;
+#       python = pkgs.python3;
+#
+#       pythonEnv =
+#         # Assert that versions from nixpkgs matches what's described in requirements.txt
+#         # In projects that are overly strict about pinning it might be best to remove this assertion entirely.
+#         # assert project.validators.validateVersionConstraints { inherit python; } == { };
+#         (
+#           # Render requirements.txt into a Python withPackages environment
+#           pkgs.python3.withPackages (project.renderers.withPackages { inherit python; })
+#         );
+#
+#     in
+#     {
+#       devShells.x86_64-linux.default = pkgs.mkShell { packages = [ pythonEnv ]; };
+#     };
+# }
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    python-3-12-8-nixpkgs.url = "github:NixOS/nixpkgs/3df3c47c19dc90fec35359e89ffb52b34d2b0e94";
-    poetry2nix.url = "github:nix-community/poetry2nix";
+    python-3-11-11-nixpkgs.url = "github:NixOS/nixpkgs/d98abf5cf5914e5e4e9d57205e3af55ca90ffc1d";
   };
 
   outputs =
@@ -15,26 +44,22 @@
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
-      python = inputs.python-3-12-8-nixpkgs.legacyPackages.${system}.python3;
-      poetry = inputs.nixpkgs.legacyPackages.${system}.poetry;
-      inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
+      python = inputs.python-3-11-11-nixpkgs.legacyPackages.${system}.python3;
     in
     {
       devShells.${system} = {
         default = pkgs.mkShell {
           packages = [
             python
-            poetry
           ];
           shellHook = ''
             # Set the PS1 variable to indicate dev environment
-            PS1="\[\033[1m\]\[$(tput setaf 51)\][\[\e]0;\u@devshell: \w\a\]\u@devshell:\w]\$\[\033[0m\] "
-            eval $(poetry env activate)
+            PS1='\[\e[38;5;147m\][\u@\h\[\e[38;5;250m\]:\[\e[38;5;75m\]\w\[\e[38;5;147m\]]\[\e[38;5;250m\]\$ \[\e[0m\]'
+            source .venv/bin/activate
+            uv sync
+            clear
           '';
-          installPhase = ''
-            poetry install
-          '';
-          PYTHONPATH=self;
+         PYTHONPATH=self;
         };
 
       };
