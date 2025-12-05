ee24998aac847f4f6af662d852981fa7efe4caa2
Luciism <lucism.dev@gmail.com>
2025-12-05 20:58:57 +1300

    fix: make mostplayed graph more accurate

--- apps/bot/render/mostplayed.py
+++ apps/bot/render/mostplayed.py
@@ -9,6 +9,7 @@ bg = BackgroundImageLoader(dir="mostplayed")
 
 BAR_COLOR = (45, 45, 255, 127)
 BAR_POSITIONS = [(97, 354), (220, 354), (343, 354), (466, 354)]
+MAX_BAR_HEIGHT = 250 
 
 @to_thread
 def render_mostplayed(
@@ -19,17 +20,21 @@ def render_mostplayed(
     hypixel_data = hypixel.get_player_dict(hypixel_data)
     bedwars_data: dict = hypixel_data.get('stats', {}).get('Bedwars', {})
 
-    solos = bedwars_data.get('eight_one_games_played_bedwars', 1)
-    doubles = bedwars_data.get('eight_two_games_played_bedwars', 1)
-    threes = bedwars_data.get('four_three_games_played_bedwars', 1)
-    fours = bedwars_data.get('four_four_games_played_bedwars', 1)
+    solos: int = bedwars_data.get('eight_one_games_played_bedwars', 0)
+    doubles: int = bedwars_data.get('eight_two_games_played_bedwars', 0)
+    threes: int = bedwars_data.get('four_three_games_played_bedwars', 0)
+    fours: int = bedwars_data.get('four_four_games_played_bedwars', 0)
 
     rank_info = hypixel.get_rank_info(hypixel_data)
 
     # Get ratio
-    numbers = [int(solos), int(doubles), int(threes), int(fours)]
-    total = sum(numbers)
-    ratios = [num / total for num in numbers]
+    values: list[int] = [solos, doubles, threes, fours]
+    normal = 1 / sum(values)
+
+    heights = [normal * value * MAX_BAR_HEIGHT for value in values]
+
+    # Make sure highest bar always reaches top
+    height_multiplier = MAX_BAR_HEIGHT / max(heights)
 
     # Open Images
     im = ImageRender(bg.load_background_image(uuid, {
@@ -38,17 +43,10 @@ def render_mostplayed(
     bar_plot_img = Image.new('RGBA', (640, 420), (0, 0, 0, 0))
     draw = ImageDraw.Draw(bar_plot_img)
 
-    # Ensure the bars are under 500 pixels in height and still relative
-    if max(ratios) * 500 > 250:
-        difference = (max(ratios) * 500) - 250
-        ratios = [value - (difference / 500)
-                  if value - (difference / 500) > 0 else 0 for value in ratios]
 
     # Draw the bars
-    for i, value in enumerate(ratios):
-        height = 250 if value * 500 > 250 else value * 500
-
-        top_left = (BAR_POSITIONS[i][0], BAR_POSITIONS[i][1] - height)
+    for i, height in enumerate(heights):
+        top_left = (BAR_POSITIONS[i][0], BAR_POSITIONS[i][1] - height * height_multiplier)
         bottom_right = (BAR_POSITIONS[i][0] + 77, BAR_POSITIONS[i][1])
         draw.rectangle((top_left, bottom_right), fill=BAR_COLOR)
 
