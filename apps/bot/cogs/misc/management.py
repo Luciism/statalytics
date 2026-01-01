44dd683e89cd9784320cee7b2118f5ea4d1e9789
Luciism <lucism.dev@gmail.com>
2026-01-01 18:12:21 +1300

    feat: add help text command

--- apps/bot/cogs/misc/management.py
+++ apps/bot/cogs/misc/management.py
@@ -1,17 +1,26 @@
 from discord.ext import commands
 
+import statalib as lib
+import helper
+
 
 class Management(commands.Cog):
-    def __init__(self, client):
-        self.client: commands.Bot = client
-
+    def __init__(self, client: helper.Client):
+        self.client: helper.Client = client
+        _ = self.client.remove_command("help")
 
     @commands.command()
     @commands.is_owner()
-    async def sync(self, ctx: commands.Context):
-        await self.client.tree.sync()
-        await ctx.send('Successfully synced slash command tree!')
+    async def sync(self, ctx: commands.Context[helper.Client]):
+        _app_commands = await self.client.tree.sync()
+        _ = await ctx.send('Successfully synced slash command tree!')
+
+    @commands.command()
+    async def help(self, ctx: commands.Context[helper.Client]):
+        embed = lib.Embeds.help.help()
+
+        _ = await ctx.send(embed=embed, view=lib.shared_views.HelpMenuButtons())
 
 
-async def setup(client: commands.Bot) -> None:
+async def setup(client: helper.Client) -> None:
     await client.add_cog(Management(client))
