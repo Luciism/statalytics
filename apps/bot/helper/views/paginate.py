a8df071e6e14ec18317923951af0aa565c54ea7a
Luciism <lucism.dev@gmail.com>
2026-01-02 21:51:41 +1300

    feat: add skip left, skip left, and goto pagination

--- apps/bot/helper/views/paginate.py
+++ apps/bot/helper/views/paginate.py
@@ -1,8 +1,9 @@
-from typing import Self, Callable, Awaitable
+from typing import Self, Callable, Awaitable, final
 import discord
+from discord.ui import TextInput
 from typing_extensions import override
 
-from ._custom import CustomBaseView
+from ._custom import CustomBaseModal, CustomBaseView
 from .. import emoji
 
 
@@ -41,6 +42,107 @@ class NextPageBtn(discord.ui.Button["PaginationView"]):
         await self.view.callback(view.page)
 
 
+class SkipLeftBtn(discord.ui.Button["PaginationView"]):
+    def __init__(self) -> None:
+        super().__init__(
+            style=discord.ButtonStyle.gray, emoji=emoji.SKIP_LEFT, custom_id="skip_left_btn"
+        )
+
+    @override
+    async def callback(self, interaction: discord.Interaction) -> None:
+        if self.view is None:
+            return
+
+        view = self.view.replicate(page=1)
+        _ = await self.view.base_interaction.edit_original_response(view=view)
+
+        await interaction.response.defer()
+        await self.view.callback(view.page)
+
+
+class SkipRightBtn(discord.ui.Button["PaginationView"]):
+    def __init__(self) -> None:
+        super().__init__(
+            style=discord.ButtonStyle.gray, emoji=emoji.SKIP_RIGHT, custom_id="skip_right_btn"
+        )
+
+    @override
+    async def callback(self, interaction: discord.Interaction) -> None:
+        if self.view is None:
+            return
+
+        if self.view.total_pages:
+            page = self.view.total_pages
+        else:
+            page = self.view.page + 10
+
+        view = self.view.replicate(page)
+        _ = await self.view.base_interaction.edit_original_response(view=view)
+
+        await interaction.response.defer()
+        await self.view.callback(view.page)
+
+
+@final
+class GoToPageModal(CustomBaseModal, title='Skip to page #'):
+    def __init__(
+        self,
+        update_page_callable: Callable[[int], Awaitable[None]],
+        total_pages: int | None=None,
+    ) -> None:
+        super().__init__()
+        self.update_page = update_page_callable
+        self.total_pages: int | None = total_pages
+
+    page_number: TextInput['GoToPageModal'] = TextInput(
+        label='Page Number',
+        placeholder='1',
+        style=discord.TextStyle.short,
+        min_length=1
+    )
+
+    @override
+    async def on_submit(self, interaction: discord.Interaction) -> None:
+        await interaction.response.defer()
+
+        try:
+            page = int(self.page_number.value)
+        except ValueError:
+            return
+
+        await self.update_page(page)
+
+
+
+class GoToBtn(discord.ui.Button["PaginationView"]):
+    def __init__(self, page: int, total_pages: int | None) -> None:
+        super().__init__(
+            label=f"Page {page}/{total_pages}" if total_pages else f"Page {page}",
+            style=discord.ButtonStyle.gray,
+            # emoji=emoji.HASH,
+            custom_id="goto_btn"
+        )
+
+    @override
+    async def callback(self, interaction: discord.Interaction) -> None:
+        if self.view is None:
+            return
+
+        async def update_page(page: int) -> None:
+            if self.view is None:
+                return
+
+            if self.view.total_pages and page > self.view.total_pages:
+                page = self.view.total_pages
+
+            view = self.view.replicate(max(page, 1))
+            _ = await self.view.base_interaction.edit_original_response(view=view)
+
+            await self.view.callback(view.page)
+
+        await interaction.response.send_modal(GoToPageModal(update_page, self.view.total_pages))
+
+
 class PaginationView(CustomBaseView):
     def __init__(
         self,
@@ -58,24 +160,24 @@ class PaginationView(CustomBaseView):
         self.base_interaction: discord.Interaction = interaction
         self.callback: Callable[[int], Awaitable[None]] = callback 
 
-        btn_label = f"Page {page}/{total_pages}" if total_pages else f"Page {page}"
-
-        page_info_btn: discord.ui.Button[Self] = discord.ui.Button(
-            style=discord.ButtonStyle.gray, label=btn_label
-        )
-        page_info_btn.disabled = True
-
         prev_btn = PreviousPageBtn()
+        skip_left_btn = SkipLeftBtn()
         if page == 1:
             prev_btn.disabled = True
+            skip_left_btn.disabled = True
 
         next_btn = NextPageBtn()
+        skip_right_btn = SkipRightBtn()
 
         if page == total_pages:
             next_btn.disabled = True
+            skip_right_btn.disabled = True
+
+        _ = self.add_item(skip_left_btn)
         _ = self.add_item(prev_btn)
-        _ = self.add_item(page_info_btn)
+        _ = self.add_item(GoToBtn(page, total_pages))
         _ = self.add_item(next_btn)
+        _ = self.add_item(skip_right_btn)
 
     def replicate(self, page: int) -> "PaginationView":
         return PaginationView(
