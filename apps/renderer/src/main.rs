2d1bcaa704bd33216cab59a13f339869623707d4
Luciism <lucism.dev@gmail.com>
2026-03-23 08:50:01 +1300

    chore(renderer): add templates for all layouts

--- apps/renderer/src/main.rs
+++ apps/renderer/src/main.rs
@@ -1,16 +1,12 @@
 use log::info;
 
 use fractyl_renderer::http::AxumRenderingServer;
-use fractyl_renderer::schema;
 
 #[tokio::main]
 async fn main() { 
     colog::init();
 
-    let server = AxumRenderingServer::new().add_renderer(
-        schema::load_schema_from_file("./exports/general-bedwars-stats/schema.json").unwrap(),
-        "/bedwars",
-    );
+    let server = AxumRenderingServer::new().discover_templates().unwrap();
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3001").await.unwrap();
     info!("Server running on http://localhost:3001");
